e127fdbb94edcc3c1cf222c87f9640a2b2a7f4cf
Nayan Mondal <n007kingkong@gmail.com>
2026-06-01 23:50:08 +0600

    sending the new user data to the backend

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -4,14 +4,41 @@ const Users = ({userPromise}) => {
 
     const usersData = use(userPromise);
     console.log(usersData);
+
+    const handleAddUser =(e)=>{
+       e.preventDefault();
+       const name = e.target.name.value;
+       const email = e.target.email.value;
+       const role = e.target.role.value;
+
+       console.log(name,role,email);
+
+       const newUser = {name,email,role};
+
+       fetch('http://localhost:4000/users',{
+        method:'POST',
+        headers: {
+           'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(newUser)
+       })
+
+    }
     return (
         <div>
            {
             usersData.map(user => <ul>
                 <li>{user.name}</li>
-                <li>{user.status}</li>
+                
             </ul>)
            }
+
+           <form onSubmit={handleAddUser}>
+               <input type="text" name="name" id="" placeholder='Enter user name'/><br />
+               <input type="text" name="email" id="" placeholder='Enter user email'/><br />
+               <input type="text" name="role" id="" placeholder='Enter user role'/><br />
+               <button type="submit">Add user</button>
+           </form>
         </div>
     );
 };
